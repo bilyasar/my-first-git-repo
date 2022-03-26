38d595d38b497fd6a20632281c651c58c8449a9e
Bilal YASAR <bilyasar@gmail.com>
2022-03-26 15:55:51 -0400

    made some changes

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -1,5 +1,7 @@
 public class FirstJavaClass {
     public static void main(String[] args) {
         System.out.println("Learning GitHub");
+        System.out.println("adding a new code");
+        System.out.println("this is another one");
     }
 }
